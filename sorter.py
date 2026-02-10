dc3e40b0794d22f023e8e1af0200cb81c286737b
hi123751 <claussonalexander@gmail.com>
2026-02-09 19:53:18 -0800

    Update sorter.py

--- sorter.py
+++ sorter.py
@@ -1,2 +1,13 @@
 from glob import glob
-jpg = glob('*.JPG')
+import shutil
+import os
+home = os.environ['USERPROFILE']
+jpg = glob(home / 'Downloads/*.jpg')
+des = 'jpg/'
+for file in jpg:
+  shuthil.move(file, des)
+py = glob(home / 'Downloads/*.py')
+des2 = 'python/'
+for file1 in py:
+  shutil.move(file1, des2)
+exe = glob(home / 'Downloads/*.exe)
